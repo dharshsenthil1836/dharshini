a98c4143c2f98726ba9b0e6805fb91cf9df10b41
dharshini <dharshini93225287@gmail.com>
2025-05-16 16:02:21 +0530

    day3

---
+++ script.js
@@ -0,0 +1,34 @@
+var a= 55;
+
+var b= 45;
+console.log(a)
+console.log(b)
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+function add(){
+   var a=10;
+   var b=20;
+   console.log (a+b);
+}
+add()
+
+
+
+function add(a,b){
+
+    console.log(a+b);
+}
+add(10,2)
+add(7, 9)
+add(8,7)
+
+
+
+
+
+
